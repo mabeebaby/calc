4860a2e09939f0cfd4f3470208af596edc37f6f8
MabeeBaby <mabeebabyow@gmail.com>
2020-09-09 23:17:20 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+
+print(Back.BLACK)
+print(Fore.GREEN)
+
+what = input('Что будете делать?: +, -, *, /: ')
+
+print(Back.RED)
+print(Fore.YELLOW)
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+
+if what == '+':
+    result = num1 + num2
+
+elif what == '-':
+    result = num1 - num2
+
+elif what == '*':
+    result = num1 * num2
+
+elif what == '/':
+    result = num1 / num2
+
+
+print(Back.GREEN)
+print(Fore.RED)
+print('Результат: ' + str(result))
